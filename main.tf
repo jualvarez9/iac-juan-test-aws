7400ae2959a8c1ef70f3783f8614363f4dcd099f
Juan Alvarez <j.alvarezg@eurodivisas.com>
2025-07-24 12:00:42 +0200

    Creacion EC2 Prueba 2

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "aws_instance" "prueba1" {
   }
   
 }
+
+resource "aws_instance" "prueba2" {
+  ami           = var.ami["apache"] 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Prueba2"
+  }
+  
+}
